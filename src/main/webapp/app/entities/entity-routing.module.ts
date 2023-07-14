0cec5835c1bace284842e64ee6482d8dc9df5b44
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-07-14 17:45:41 +0300

    Add test entinty

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -1,9 +1,14 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'test-entity',
+        data: { pageTitle: 'mmseApp.testEntity.home.title' },
+        loadChildren: () => import('./test-entity/test-entity.routes'),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
