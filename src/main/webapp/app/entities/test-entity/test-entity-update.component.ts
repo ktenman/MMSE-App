136432eb24d79501292bdf29d04c5155dc89fa93
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-07-18 02:06:52 +0300

    Refactor test entity update component

--- src/main/webapp/app/entities/test-entity/test-entity-update.component.ts
+++ src/main/webapp/app/entities/test-entity/test-entity-update.component.ts
@@ -1,17 +1,17 @@
-import { computed, defineComponent, inject, ref, Ref } from 'vue';
-import { useI18n } from 'vue-i18n';
-import { useRoute, useRouter } from 'vue-router';
-import { useVuelidate } from '@vuelidate/core';
+import {computed, defineComponent, inject, ref, Ref} from 'vue';
+import {useI18n} from 'vue-i18n';
+import {useRoute, useRouter} from 'vue-router';
+import {useVuelidate} from '@vuelidate/core';
 
-import { useValidation, useDateFormat } from '@/shared/composables';
-import { useAlertService } from '@/shared/alert/alert.service';
+import {useDateFormat, useValidation} from '@/shared/composables';
+import {useAlertService} from '@/shared/alert/alert.service';
 
 import UserService from '@/entities/user/user.service';
-import { ITestEntity, TestEntity } from '@/shared/model/test-entity.model';
+import {ITestEntity, TestEntity} from '@/shared/model/test-entity.model';
 import TestEntityService from './test-entity.service';
 
 export default defineComponent({
-  compatConfig: { MODE: 3 },
+  compatConfig: {MODE: 3},
   name: 'TestEntityUpdate',
   setup() {
     const testEntityService = inject('testEntityService', () => new TestEntityService());
@@ -28,7 +28,7 @@ export default defineComponent({
 
     const previousState = () => router.go(-1);
 
-    const retrieveTestEntity = async testEntityId => {
+    const retrieveTestEntity = async (testEntityId) => {
       try {
         const res = await testEntityService().find(testEntityId);
         res.createdAt = new Date(res.createdAt);
@@ -39,21 +39,22 @@ export default defineComponent({
       }
     };
 
+    const initRelationships = async () => {
+      try {
+        const res = await userService().retrieve();
+        users.value = res.data;
+      } catch (error) {
+        alertService.showHttpError(error.response);
+      }
+    };
+
     if (route.params?.testEntityId) {
       retrieveTestEntity(route.params.testEntityId);
     }
 
-    const initRelationships = () => {
-      userService()
-        .retrieve()
-        .then(res => {
-          users.value = res.data;
-        });
-    };
-
     initRelationships();
 
-    const { t: t$ } = useI18n();
+    const {t: t$} = useI18n();
     const validations = useValidation();
     const validationRules = {
       createdAt: {
@@ -69,47 +70,32 @@ export default defineComponent({
     v$.value.$validate();
 
     return {
-      testEntityService,
-      alertService,
       testEntity,
       previousState,
       isSaving,
       currentLanguage,
       users,
       v$,
-      ...useDateFormat({ entityRef: testEntity }),
+      ...useDateFormat({entityRef: testEntity}),
       t$,
+      save: async () => {
+        isSaving.value = true;
+        try {
+          let response;
+          if (testEntity.value.id) {
+            response = await testEntityService().update(testEntity.value);
+            alertService.showInfo(t$('mmseApp.testEntity.updated', {param: response.id}));
+          } else {
+            response = await testEntityService().create(testEntity.value);
+            alertService.showSuccess(t$('mmseApp.testEntity.created', {param: response.id}).toString());
+          }
+          previousState();
+        } catch (error) {
+          alertService.showHttpError(error.response);
+        } finally {
+          isSaving.value = false;
+        }
+      },
     };
-  },
-  created(): void {},
-  methods: {
-    save(): void {
-      this.isSaving = true;
-      if (this.testEntity.id) {
-        this.testEntityService()
-          .update(this.testEntity)
-          .then(param => {
-            this.isSaving = false;
-            this.previousState();
-            this.alertService.showInfo(this.t$('mmseApp.testEntity.updated', { param: param.id }));
-          })
-          .catch(error => {
-            this.isSaving = false;
-            this.alertService.showHttpError(error.response);
-          });
-      } else {
-        this.testEntityService()
-          .create(this.testEntity)
-          .then(param => {
-            this.isSaving = false;
-            this.previousState();
-            this.alertService.showSuccess(this.t$('mmseApp.testEntity.created', { param: param.id }).toString());
-          })
-          .catch(error => {
-            this.isSaving = false;
-            this.alertService.showHttpError(error.response);
-          });
-      }
-    },
   },
 });
