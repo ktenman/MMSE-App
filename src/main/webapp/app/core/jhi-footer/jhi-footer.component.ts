224854214607379eb4891e2895dc4b82754edaf1
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-07-18 01:41:28 +0300

    Hide footer

--- src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
@@ -1,12 +1,20 @@
-import {defineComponent} from 'vue';
+import {computed, defineComponent} from 'vue';
 import {useI18n} from 'vue-i18n';
 
 export default defineComponent({
-  compatConfig: {MODE: 3},
+  compatConfig: { MODE: 3 },
   name: 'MmseFooter',
   setup() {
+    const { t } = useI18n();
+
+    const copyright = computed(() => {
+      const currentYear = new Date().getFullYear();
+      return `${t('footer')} © ${currentYear}`;
+    });
+
     return {
-      t$: useI18n().t,
+      t,
+      copyright,
     };
   },
 });
