1c5efa1e9a43903cfdaf5cd3cd4f70974af247d1
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-07-18 23:27:05 +0300

    Fix home page

--- src/main/webapp/app/core/home/home.component.ts
+++ src/main/webapp/app/core/home/home.component.ts
@@ -1,39 +1,22 @@
-import {ComputedRef, defineComponent, inject, ref} from 'vue';
+import {ComputedRef, defineComponent, inject} from 'vue';
 import {useI18n} from 'vue-i18n';
 
 import LoginService from '@/account/login.service';
-import QuizService from '@/quiz/quiz.service'; // import QuizService
 
 export default defineComponent({
   compatConfig: {MODE: 3},
   setup() {
     const loginService = inject<LoginService>('loginService');
-    const quizService = inject('quizService', () => new QuizService()); // inject QuizService
     const authenticated = inject<ComputedRef<boolean>>('authenticated');
     const username = inject<ComputedRef<string>>('currentUsername');
-
-    const question = ref(''); // store the current question
-
     const openLogin = () => {
       loginService.openLogin();
-    };
-
-    // fetch the first question and store it in the question ref
-    const startQuiz = async () => {
-      try {
-        const res = await quizService.getNextQuestion();
-        question.value = res.data.question;
-      } catch (error) {
-        console.log('Error', error);
-      }
     };
 
     return {
       authenticated,
       username,
-      question,
       openLogin,
-      startQuiz,
       t$: useI18n().t,
     };
   },
