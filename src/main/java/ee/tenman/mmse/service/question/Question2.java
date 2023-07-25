1e372a8c3a3fb4d90328712a1afb6e077c4fa4ee
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-07-25 22:33:13 +0300

    Fix 2nd question implementation

--- src/main/java/ee/tenman/mmse/service/question/Question2.java
+++ src/main/java/ee/tenman/mmse/service/question/Question2.java
@@ -2,19 +2,30 @@ package ee.tenman.mmse.service.question;
 
 import ee.tenman.mmse.domain.UserAnswer;
 import ee.tenman.mmse.domain.enumeration.QuestionId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 @Component
 public class Question2 implements Question {
 
     private static final String QUESTION_TEXT = "What is the current date?";
     private final QuestionId questionId = QuestionId.QUESTION_2;
+
+    @Autowired
+    private Clock clock;
 
     @Override
     public String getQuestionText() {
@@ -31,7 +42,8 @@ public class Question2 implements Question {
         LocalDate localDate = userAnswer.getCreatedAt().atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate userAnswerDate;
         try {
-            userAnswerDate = LocalDate.parse(userAnswer.getAnswerText(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String userAnswerText = userAnswer.getAnswerText().replaceAll("(st|nd|rd|th)", "");
+            userAnswerDate = LocalDate.parse(userAnswerText, DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH));
         } catch (DateTimeParseException e) {
             return false;
         }
@@ -45,6 +57,43 @@ public class Question2 implements Question {
 
     @Override
     public List<String> getAnswerOptions() {
-        return null;
+        // Get today's date
+        LocalDate today = LocalDate.now(clock);
+
+        // Format it as a string in the format '25th May 2023'
+        String formattedToday = formatToOrdinalDate(today);
+
+        // Generate three incorrect dates
+        List<String> incorrectDates = Arrays.asList(
+            formatToOrdinalDate(today.minus(1, ChronoUnit.MONTHS)),
+            formatToOrdinalDate(today.plus(1, ChronoUnit.MONTHS)),
+            formatToOrdinalDate(today.minus(1, ChronoUnit.MONTHS).plus(1, ChronoUnit.DAYS))
+        );
+
+        // Combine correct and incorrect dates into one list
+        List<String> options = new ArrayList<>(incorrectDates);
+        options.add(formattedToday);
+
+        // Randomly shuffle the options
+        Collections.shuffle(options);
+
+        return options;
+    }
+
+    private String formatToOrdinalDate(LocalDate date) {
+        int day = date.getDayOfMonth();
+        String dayWithSuffix = day + getDayOfMonthSuffix(day);
+        return dayWithSuffix + " " + date.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) + " " + date.getYear();
+    }
+
+    String getDayOfMonthSuffix(final int n) {
+        if (n < 1 || n > 31) throw new IllegalArgumentException("Illegal day of month");
+        if (n >= 11 && n <= 13) return "th";
+        return switch (n % 10) {
+            case 1 -> "st";
+            case 2 -> "nd";
+            case 3 -> "rd";
+            default -> "th";
+        };
     }
 }
--- src/test/java/ee/tenman/mmse/service/question/Question2Test.java
+++ src/test/java/ee/tenman/mmse/service/question/Question2Test.java
@@ -1,37 +1,54 @@
 package ee.tenman.mmse.service.question;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import ee.tenman.mmse.domain.UserAnswer;
 import ee.tenman.mmse.domain.enumeration.QuestionId;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.time.ZoneOffset.UTC;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class Question2Test {
 
+    @Mock
+    private Clock clock;
+
+    @InjectMocks
     private Question2 question2;
+
     private UserAnswer userAnswer;
 
     @BeforeEach
     void setUp() {
-        question2 = new Question2();
         userAnswer = new UserAnswer();
         userAnswer.setQuestionId(QuestionId.QUESTION_2);
         LocalDateTime localDateTime = LocalDateTime.of(2023, 1, 1, 0, 0);
-        userAnswer.setCreatedAt(localDateTime.toInstant(ZoneOffset.UTC));
+        userAnswer.setCreatedAt(localDateTime.toInstant(UTC));
     }
 
     @Test
     void testIsAnswerCorrect_whenTrue() {
-        userAnswer.setAnswerText("2023-01-01");
+        userAnswer.setAnswerText("1st January 2023");
         assertThat(question2.isAnswerCorrect(userAnswer)).isTrue();
     }
 
     @Test
     void testIsAnswerCorrect_whenFalse() {
-        userAnswer.setAnswerText("2023-01-02");
+        userAnswer.setAnswerText("2nd January 2023");
         assertThat(question2.isAnswerCorrect(userAnswer)).isFalse();
     }
 
@@ -48,5 +65,42 @@ class Question2Test {
     @Test
     void testGetQuestionId() {
         assertThat(question2.getQuestionId()).isEqualTo(QuestionId.QUESTION_2);
+    }
+
+    @Test
+    void testGetAnswerOptions() {
+        when(clock.instant()).thenReturn(LocalDateTime.of(2023, 1, 1, 0, 0)
+            .toInstant(UTC));
+        when(clock.getZone()).thenReturn(UTC);
+
+        List<String> answerOptions = question2.getAnswerOptions();
+        assertThat(answerOptions).hasSize(4).contains("1st January 2023");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "1, st",
+        "2, nd",
+        "3, rd",
+        "4, th",
+        "11, th",
+        "12, th",
+        "13, th",
+        "21, st",
+        "22, nd",
+        "23, rd",
+        "31, st"
+    })
+    void testGetDayOfMonthSuffix(int day, String expectedSuffix) {
+        assertThat(question2.getDayOfMonthSuffix(day)).isEqualTo(expectedSuffix);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {
+        0,
+        32
+    })
+    void testGetDayOfMonthSuffix_ThrowsException(int day) {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> question2.getDayOfMonthSuffix(day));
     }
 }
