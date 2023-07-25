7ea3697a14523a59f4e28655e6ed12d8c5582a9c
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-07-25 11:54:36 +0300

    Add submit answer possibility

--- src/main/webapp/app/entities/question/question.service.ts
+++ src/main/webapp/app/entities/question/question.service.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {IQuestion} from '@/shared/model/question.model';
+import {IAnswer} from "@/shared/model/answer.model";
 
 const baseApiUrl = 'api/question';
 
@@ -7,4 +8,13 @@ export default class QuestionService {
   public getQuestion(): Promise<IQuestion> {
     return axios.get<IQuestion>(baseApiUrl).then(res => res.data);
   }
+
+  public submitAnswer(answer: IAnswer) {
+    return axios.post('/api/answer', answer).then(res => res.data).catch(err => {
+      console.error(err);
+      throw err;
+    });
+  }
+
 }
+
---
+++ src/main/webapp/app/shared/model/answer.model.ts
@@ -0,0 +1,13 @@
+import {QuestionId} from "@/shared/model/enumerations/question-id.model";
+
+export interface IAnswer {
+  answerText?: string;
+  questionId?: QuestionId;
+}
+
+export class Answer implements IAnswer {
+  constructor(
+    public answerText?: string,
+    public questionId?: QuestionId
+  ) {}
+}
