8367baf11dbddb99e70d76e01a5a38c2d22767a4
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-11-07 06:42:48 +0200

    Fix layers

--- src/main/java/ee/tenman/mmse/service/openai/OpenAiClient.java
+++ src/main/java/ee/tenman/mmse/service/openai/OpenAiClient.java
@@ -1,11 +1,10 @@
 package ee.tenman.mmse.service.openai;
 
-import ee.tenman.mmse.config.OpenAiFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "openai", url = "https://api.openai.com", configuration = OpenAiFeignConfig.class)
+@FeignClient(name = "openai", url = "https://api.openai.com")
 public interface OpenAiClient {
 
     @PostMapping("/v1/chat/completions")
