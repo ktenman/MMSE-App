992f6c0ab3533a9cc4dfe2fa3303110e23f4d81b
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-04-04 03:30:04 +0300

    Add router check to conditionally render components based on test route

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,4 +1,4 @@
-import { defineComponent, provide } from 'vue';
+import { computed, defineComponent, provide } from 'vue';
 import { useI18n } from 'vue-i18n';
 import Ribbon from '@/core/ribbon/ribbon.vue';
 import JhiFooter from '@/core/jhi-footer/jhi-footer.vue';
@@ -8,6 +8,7 @@ import LoginForm from '@/account/login-form/login-form.vue';
 import { useAlertService } from '@/shared/alert/alert.service';
 
 import '@/shared/config/dayjs';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   compatConfig: { MODE: 3 },
@@ -20,9 +21,16 @@ export default defineComponent({
   },
   setup() {
     provide('alertService', useAlertService());
+    const router = useRouter();
+
+    const isTest = computed(() => {
+      const testRoutes = ['/test'];
+      return testRoutes.includes(router.currentRoute.value.path);
+    });
 
     return {
-      t$: useI18n().t
+      t$: useI18n().t,
+      isTest
     };
   },
 });
--- src/main/webapp/app/app.vue
+++ src/main/webapp/app/app.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <ribbon></ribbon>
+    <ribbon v-if="!isTest"></ribbon>
     <div id="app-header">
-      <jhi-navbar></jhi-navbar>
+      <jhi-navbar v-if="!isTest"></jhi-navbar>
     </div>
     <div class="container-fluid">
       <div class="card jh-card">
         <router-view></router-view>
       </div>
-      <b-modal id="login-page" hide-footer lazy>
+      <b-modal v-if="!isTest" id="login-page" hide-footer lazy>
         <template #modal-title>
           <span data-cy="loginTitle" id="login-title" v-text="t$('login.title')"></span>
         </template>
         <login-form></login-form>
       </b-modal>
 
-      <jhi-footer></jhi-footer>
+      <jhi-footer v-if="!isTest"></jhi-footer>
     </div>
   </div>
 </template>
